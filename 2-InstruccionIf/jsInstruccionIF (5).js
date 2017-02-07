cfd0f6e223b1d6b430f209e5cac97b3472b4236a
alumno <alumno>
2017-02-07 12:18:29 -0300

    Ejercicios if 1,2,3,4,5 y 6

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad=document.getElementById("edad").value;
+    if(edad>=13&&edad<=17)
+    {
+        alert("eres adolescente")
+    }
+    else{
+        alert("no eres adolescente")
+    }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
+    var edad=document.getElementById("edad").value;
+    if(edad>=13&&edad<=17){
+        alert("eres adolescente")
+    }
+    else{
+        if(edad<13){
+            alert("eres niño")
+        }
+        else{
+            alert("eres mayor de edad")}
+    }
+
 
 
 
